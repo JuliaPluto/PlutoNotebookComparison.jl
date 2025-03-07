3ecd95880e6e0fc7ee525d430232b148d02e2ad4
Fons van der Plas <fonsvdplas@gmail.com>
2025-03-07 10:57:00 +0100

    Update CI.jl

--- src/CI.jl
+++ src/CI.jl
@@ -35,35 +35,36 @@ function compare_PR(dir::AbstractString;
     all_notebooks = PlutoSliderServer.find_notebook_files_recursive(dir)
     
     for path in all_notebooks
-        
+        @info "Notebook" path
         
         delta = find(d -> unsafe_string(d.new_file.path) == path, pr_deltas)
         
-        if delta !== nothing
-            if Glitter.is_modified(delta)
-                contents_old = Glitter.contents(repo, delta.old_file)
-                contents_new = Glitter.contents(repo, delta.new_file)
-                
-                path_old = unsafe_string(delta.old_file.path)
-                path_new = unsafe_string(delta.new_file.path)
-                
-                statefile_old = PlutoNotebookComparison.get_statefile(sources_old, path_old, contents_old)
-                statefile_new = PlutoNotebookComparison.get_statefile(sources_new, path_new, contents_new)
-                
-                @info "Search results" path statefile_old statefile_new
+        if delta !== nothing && Glitter.is_modified(delta)
+            contents_old = Glitter.contents(repo, delta.old_file)
+            contents_new = Glitter.contents(repo, delta.new_file)
+            
+            path_old = unsafe_string(delta.old_file.path)
+            path_new = unsafe_string(delta.new_file.path)
+            
+            statefile_old = PlutoNotebookComparison.get_statefile(sources_old, path_old, contents_old)
+            statefile_new = PlutoNotebookComparison.get_statefile(sources_new, path_new, contents_new)
+            
+            @info "Search results" path statefile_old statefile_new
 
-                @assert statefile_old.found
-                @assert statefile_new.found
-                
-                state_old = Pluto.unpack(statefile_old.result)
-                state_new = Pluto.unpack(statefile_new.result)
+            @assert statefile_old.found
+            @assert statefile_new.found
+            
+            state_old = Pluto.unpack(statefile_old.result)
+            state_new = Pluto.unpack(statefile_new.result)
 
-                drama_context = PlutoNotebookComparison.get_drama_context(state_old, state_new)
-                
-                for f in drama_checkers
-                    f(drama_context)
-                end
+            drama_context = PlutoNotebookComparison.get_drama_context(state_old, state_new)
+            
+            for f in drama_checkers
+                @info "checking" path f
+                f(drama_context)
             end
+        else
+            @info "skipping..." path
         end
     end
 
