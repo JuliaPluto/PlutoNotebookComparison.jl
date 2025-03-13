eae0027bc05df2b045f5d1ff449995a20da5c564
Fons van der Plas <fonsvdplas@gmail.com>
2025-03-13 14:48:43 +0100

    chatgpt didnt work

--- test/CI.jl
+++ test/CI.jl
@@ -1,28 +1,36 @@
 using Test
-
 using PlutoNotebookComparison
 
+good1 = checkout_test_repo(; branch_name="good1")
+bad_error = checkout_test_repo(; branch_name="bad-error")
+# main = checkout_test_repo(; branch_name="main")
 
+@testset "CI for PR" begin
 
+    sources_old = [
+        # PSSCache("pluto_state_cache")
+        # WebsiteDir("gh_pages_dir")
+        # WebsiteAddress("https://biaslab.github.io/BMLIP-colorized/")
+        SafePreview()
+    ]
 
-good1 = checkout_test_repo(; branch_name="origin/good1")
-main = checkout_test_repo(; branch_name="origin/main")
+    sources_new = [
+        # PSSCache("pluto_state_cache")
+        RunWithPlutoSliderServer()
+    ]
 
-
-
-sources_old = [
-    # PSSCache("pluto_state_cache")
-    # WebsiteDir("gh_pages_dir")
-    # WebsiteAddress("https://biaslab.github.io/BMLIP-colorized/")
-    SafePreview()
-]
-
-sources_new = [
-    # PSSCache("pluto_state_cache")
-    RunWithPlutoSliderServer()
-]
-
-PlutoNotebookComparison.compare_PR(good1;
-    sources_old,
-    sources_new,
-)
+    @testset "good" begin
+        PlutoNotebookComparison.compare_PR(good1;
+            sources_old,
+            sources_new,
+        )
+        @test true
+    end
+    
+    @testset "bad error" begin
+        @test_throws Exception PlutoNotebookComparison.compare_PR(bad_error;
+            sources_old,
+            sources_new,
+        )
+    end
+end
--- test/checkout test repo.jl
+++ test/checkout test repo.jl
@@ -1,32 +1,9 @@
 using LibGit2
 
-function checkout_test_repo(; branch_name::String="origin/main", repo_url::String="https://github.com/JuliaPluto/PlutoNotebookComparison-test")
-    # thx chatght
-    
+function checkout_test_repo(; branch_name::String="main", repo_url::String="https://github.com/JuliaPluto/PlutoNotebookComparison-test")
 	testdir = mktempdir()
-	
-	# Clone the repo (shallow default)
-	repo = LibGit2.clone(repo_url, testdir)
-	
-	remote_branch = LibGit2.lookup_branch(repo, branch_name, true)
-	@assert remote_branch !== nothing "Remote branch $branch_name not found after fetch."
-	
-	# Peel the branch to the commit it points to
-	commit = LibGit2.peel(LibGit2.GitCommit, remote_branch)
-    
-    
-    # Local branch name (strip "origin/" if present)
-    local_branch_name = basename(branch_name)
-    
-    # Only create it if it doesn't already exist
-    existing_local_branch = LibGit2.lookup_branch(repo, local_branch_name, false)
-    if existing_local_branch === nothing
-        LibGit2.create_branch(repo, local_branch_name, commit)
-    end
-	
-	# Set HEAD to the local branch and check it out
-	LibGit2.head!(repo, remote_branch)
-	LibGit2.checkout!(repo; force=true)
+	opts = LibGit2.CloneOptions(checkout_branch=Base.unsafe_convert(Cstring, branch_name))
+	repo = LibGit2.clone(repo_url, testdir, opts)
 
     testdir
 end
