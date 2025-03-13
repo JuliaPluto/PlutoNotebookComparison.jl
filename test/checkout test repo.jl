cdfa5a5d1024cb8de3f602a86719191da48df57f
Fons van der Plas <fonsvdplas@gmail.com>
2025-03-13 12:44:03 +0100

    Update checkout test repo.jl

--- test/checkout test repo.jl
+++ test/checkout test repo.jl
@@ -13,9 +13,16 @@ function checkout_test_repo(; branch_name::String="origin/main", repo_url::Strin
 	
 	# Peel the branch to the commit it points to
 	commit = LibGit2.peel(LibGit2.GitCommit, remote_branch)
-	
-	# Create a local branch "good1" from the commit
-	LibGit2.create_branch(repo, basename(branch_name), commit)
+    
+    
+    # Local branch name (strip "origin/" if present)
+    local_branch_name = basename(branch_name)
+    
+    # Only create it if it doesn't already exist
+    existing_local_branch = LibGit2.lookup_branch(repo, local_branch_name, false)
+    if existing_local_branch === nothing
+        LibGit2.create_branch(repo, local_branch_name, commit)
+    end
 	
 	# Set HEAD to the local branch and check it out
 	LibGit2.head!(repo, remote_branch)
