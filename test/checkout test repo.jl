1ee098d829ab0502b3727b23789e3fbf3e97c397
Fons van der Plas <fonsvdplas@gmail.com>
2025-03-13 14:50:31 +0100

    Update checkout test repo.jl

--- test/checkout test repo.jl
+++ test/checkout test repo.jl
@@ -1,9 +1,11 @@
 using LibGit2
 
 function checkout_test_repo(; branch_name::String="main", repo_url::String="https://github.com/JuliaPluto/PlutoNotebookComparison-test")
-	testdir = mktempdir()
-	opts = LibGit2.CloneOptions(checkout_branch=Base.unsafe_convert(Cstring, branch_name))
-	repo = LibGit2.clone(repo_url, testdir, opts)
-
-    testdir
+    testdir = mktempdir()
+    opts = LibGit2.CloneOptions(checkout_branch=Base.unsafe_convert(Cstring, branch_name))
+    repo = LibGit2.clone(repo_url, testdir, opts)
+    # avoid gc
+    println(devnull, branch_name)
+    
+    return testdir
 end
